297235e364610860e31331bd52d2699aad65107e
Hjallimar <hjalle@ownit.nu>
2018-04-12 15:52:55 +0200

    funny scripts on objects

---
+++ TopDownShooter/Assets/Scripts/BoxScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxScript : MonoBehaviour {
+
+    private float force = 3;
+    public GameObject player;
+    Rigidbody2D rgb;
+    
+    // Use this for initialization
+	void Start () {
+        rgb = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(player.transform.position.x > this.transform.position.x)
+        {
+            rgb.AddForce(new Vector2(-force, 0));
+        }
+        else if (player.transform.position.x < this.transform.position.x)
+        {
+            rgb.AddForce(new Vector2(force, 0));
+        }
+        else if (player.transform.position.y > this.transform.position.y)
+        {
+            rgb.AddForce(new Vector2(0, -force));
+        }
+        if (player.transform.position.y < this.transform.position.y)
+        {
+            rgb.AddForce(new Vector2(0, force));
+        }
+    }
+}
---
+++ TopDownShooter/Assets/Scripts/StingyFLower.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StingyFLower : MonoBehaviour {
+
+    private int damage = 1;
+    private int hitPoints = 3;
+    public GameObject player;
+    Rigidbody2D playerRigd;
+
+    private void Start()
+    {
+        playerRigd = player.GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update () {
+        if(hitPoints >= 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+ 
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+           // player.takeDamage(1);
+           if(player.transform.position.x > this.transform.position.x)
+            {
+                Debug.Log("Damage taken, plant hp left: " + hitPoints);
+            }
+            hitPoints = hitPoints - 1;
+        }
+    }
+
+}
