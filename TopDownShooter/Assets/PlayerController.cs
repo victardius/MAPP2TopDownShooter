55324d248a85150d97661b25c9f5bedf1787b3c9
victardius <37775040+victardius@users.noreply.github.com>
2018-04-12 15:02:48 +0200

    Added enemy follow script

---
+++ TopDownShooter/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveForce = 5;
+    Rigidbody2D rgbd;
+
+	void Start () {
+
+        rgbd = this.GetComponent<Rigidbody2D>();
+		
+	}
+	
+	void FixedUpdate () {
+
+        Vector2 moveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"),
+            CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
+
+        Vector3 lookVec = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal_2"),
+            CrossPlatformInputManager.GetAxis("Vertical_2"), 4000);
+
+        if(lookVec.x != 0 && lookVec.y != 0)
+            transform.rotation = Quaternion.LookRotation(lookVec, Vector3.back);
+
+        rgbd.AddForce(moveVec);
+	}
+}
--- TopDownShooter/Assets/Scripts/MeleeEnemyBehaviour.cs
+++ TopDownShooter/Assets/Scripts/MeleeEnemyBehaviour.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class MeleeEnemyBehaviour : MonoBehaviour {
 
+    public Transform playerPosition;
+    public float speed;
+
+    private float xPosition, yPosition;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        xPosition = playerPosition.position.x - this.transform.position.x;
+
+        yPosition = playerPosition.position.y - this.transform.position.y;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-       // transform.Translate.
+        if (playerPosition.position.x - this.transform.position.x > 1)
+            xPosition = 1;
+        else if (playerPosition.position.x - this.transform.position.x < -1)
+            xPosition = -1;
+        else
+            xPosition = playerPosition.position.x - this.transform.position.x;
+
+
+
+        yPosition = playerPosition.position.y - this.transform.position.y;
+        
+        
+
+        transform.Translate(new Vector3(xPosition, yPosition, 0.0f) * speed * Time.deltaTime);
 
 	}
 }
