1bbd456e0dbb1513918e3b74bf2519ef6d35d391
Flargy <marlun1996@hotmail.com>
2018-04-12 15:58:05 +0200

    Added windows controlls for testing

--- TopDownShooter/Assets/PlayerController.cs
+++ TopDownShooter/Assets/PlayerController.cs
@@ -15,16 +15,27 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	void FixedUpdate () {
+#if UNITY_STANDALONE_WIN
+
+        float h = Input.GetAxisRaw("Horizontal") * moveForce;
+        float v = Input.GetAxisRaw("Vertical") * moveForce;
+
+        rgbd.AddForce(new Vector2(h, v));
+#endif
+#if UNITY_ANDROID
 
         Vector2 moveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"),
-            CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
+             CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
 
-        Vector3 lookVec = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal_2"),
-            CrossPlatformInputManager.GetAxis("Vertical_2"), 4000);
+         Vector3 lookVec = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal_2"),
+             CrossPlatformInputManager.GetAxis("Vertical_2"), 4000);
 
-        if(lookVec.x != 0 && lookVec.y != 0)
-            transform.rotation = Quaternion.LookRotation(lookVec, Vector3.back);
+         if(lookVec.x != 0 && lookVec.y != 0)
+             transform.rotation = Quaternion.LookRotation(lookVec, Vector3.back);
 
-        rgbd.AddForce(moveVec);
-	}
+         rgbd.AddForce(moveVec);
+#endif
+
+
+    }
 }
