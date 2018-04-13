f567dd6e5d14653b1101e478729dfb0d329ee004
Flargy <marlun1996@hotmail.com>
2018-04-13 11:08:43 +0200

    Working on guns

---
+++ TopDownShooter/Assets/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public float fireRate = 0;
+    public float Damage = 10;
+    public LayerMask notToHit;
+
+    float timeToFire = 0;
+    Transform firePoint;
+
+
+    void Awake () {
+        firePoint = transform.FindChild("FirePoint");
+        if (firePoint == null)
+        {
+            Debug.LogError("No firepoint");
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
