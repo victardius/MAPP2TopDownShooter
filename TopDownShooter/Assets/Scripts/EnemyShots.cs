4687482c36a38d67f32ed3f654f060c3e08bd6db
Flargy <marlun1996@hotmail.com>
2018-04-20 16:48:23 +0200

    enemy bullets now work as intended

--- TopDownShooter/Assets/Scripts/EnemyShots.cs
+++ TopDownShooter/Assets/Scripts/EnemyShots.cs
@@ -4,17 +4,47 @@ using UnityEngine;
 
 public class EnemyShots : MonoBehaviour {
 
+    public int damage = 10;
+    public LayerMask whatToHit;
     public int moveSpeed = 230;
     public float timeToLive;
 
     private Transform target;
     private Rigidbody2D rgbd;
 
+    Transform firePoint;
+    Transform firePointDirection;
+    ContactPoint2D contact;
+
+
     private void Start()
     {
         target = PlayerVariables.playerTarget;
         rgbd = this.gameObject.GetComponent<Rigidbody2D>();
+        firePoint = transform.Find("FirePoint");
+        firePointDirection = transform.Find("FirePointDirection");
         Shoot();
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 fireTargetPosition = new Vector2(firePointDirection.position.x, firePointDirection.position.y);
+        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, fireTargetPosition, 0.3f, whatToHit);
+
+        if (hit.collider != null)
+        {
+            Debug.Log("enemy shot hit");
+            if (hit.collider.gameObject.CompareTag("Walls"))
+                DestroyObject(this.gameObject);
+            else if (hit.collider.gameObject.CompareTag("Player"))
+            {
+                hit.collider.gameObject.GetComponent<PlayerVariables>().takeDamage(damage, transform);
+                DestroyObject(this.gameObject);
+            }
+            
+        }
     }
 
 
