089259c076bf4f949509a0f5dcc5ad2d55113681
victardius <37775040+victardius@users.noreply.github.com>
2018-04-20 16:24:36 +0200

    Sorted sprites and added start/end

--- TopDownShooter/Assets/Scripts/GameController.cs
+++ TopDownShooter/Assets/Scripts/GameController.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
-    
+    private void FixedUpdate()
+    {
+        if (MonsterSpawn.monstersSpawned && MonsterSpawn.numberOfMonsters == 0)
+            SceneManager.LoadScene(0);
 
-    
+    }
+
 
 }
