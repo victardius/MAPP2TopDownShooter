943d636c8123a6555145e1b0510cb99862308f00
victardius <37775040+victardius@users.noreply.github.com>
2018-04-27 18:10:19 +0200

    fixed landscape

---
+++ TopDownShooter/Assets/Scripts/ScreenLandscapeMode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenLandscapeMode : MonoBehaviour {
+
+	void Start () {
+        Screen.autorotateToLandscapeLeft = true;
+
+        Screen.autorotateToLandscapeRight = true;
+
+        Screen.autorotateToPortrait = false;
+
+        Screen.autorotateToPortraitUpsideDown = false;
+
+        Screen.orientation = ScreenOrientation.AutoRotation;
+
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+}
