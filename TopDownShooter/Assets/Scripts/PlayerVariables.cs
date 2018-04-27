4d04a90c26beda9f2ef6931d435913a436075e3e
Hjallimar <hjalle@ownit.nu>
2018-04-27 14:24:05 +0200

    You have a shield :D

--- TopDownShooter/Assets/Scripts/PlayerVariables.cs
+++ TopDownShooter/Assets/Scripts/PlayerVariables.cs
@@ -6,12 +6,17 @@ using UnityEngine.UI;
 public class PlayerVariables : MonoBehaviour {
 
     public int health = 100;
+    public int shield = 3;
+    public GameObject shieldSprite;
     public Slider healthSlider;
+    public Slider shieldSlider;
     public float pushbackForce = 10;
     public static Transform playerTarget;
 
 	void Start () {
         playerTarget = transform;
+        shieldSprite.SetActive(true);
+
         health = 100;
 	}
 	
@@ -19,19 +24,46 @@ public class PlayerVariables : MonoBehaviour {
 	void Update () {
 
         healthSlider.value = health;
+        shieldSlider.value = shield;
 
         if (health <= 0)
             GameController.missionFailed = true;
         
 	}
 
+    public void healthPack(int x)
+    {
+        if (health <= 90)
+            health += x;
+    }
+
+    public void shieldPack(int x)
+    {
+        if(shield < 3)
+        {
+            shield ++;
+            shieldSprite.SetActive(true);
+        }
+    }
+
     public void takeDamage(int amount, Transform enemy)
     {
-        health -= amount;
+        if (shield >= 1)
+        {
+            Debug.Log("Shield" + shield);
+            shield --;
+        }
+        else
+        {
+            shieldSprite.SetActive(false);
+            health -= amount;
+        }
         Vector3 dir = enemy.position - transform.position;
-
         dir = -dir.normalized;
         this.gameObject.GetComponent<Rigidbody2D>().AddForce(dir * pushbackForce);
+
+
+
 
     }
 
