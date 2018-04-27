cf8ff53ab414658a111edd5be5453e4448f5dcb2
Hjallimar <hjalle@ownit.nu>
2018-04-27 14:52:52 +0200

    Health pack working as intended

---
+++ TopDownShooter/Assets/Scripts/HealthPack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPack : MonoBehaviour {
+
+    public int health = 10;
+    public GameObject Player;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log(Player.GetComponent<PlayerVariables>().health);
+            Player.GetComponent<PlayerVariables>().healthPack(health);
+            Debug.Log(Player.GetComponent<PlayerVariables>().health);
+        }
+    }
+
+}
