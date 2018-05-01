b0a1000750ffb460b77ff3bfbc23c528834ddda9
Flargy <marlun1996@hotmail.com>
2018-05-01 15:03:53 +0200

    Added script for changing weapons (draft)

--- TopDownShooter/Assets/Scripts/Weapon.cs
+++ TopDownShooter/Assets/Scripts/Weapon.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class Weapon : MonoBehaviour {
 
     public float fireRate = 0;
-    public int damage = 10;
+    public int damagePistol = 10;
+    public int damageRifle = 10;
+    public int damageShotgun = 10;
+    public int damageSniper = 10;
     public AudioClip pistol;
+    public Sprite handGun;
+    public Sprite rifle;
+    public Sprite shotgun;
+    public Sprite sniper;
+
 
     public static int bulletDamage;
 
@@ -18,11 +26,15 @@ public class Weapon : MonoBehaviour {
     private AudioSource source;
     private float volLowRange = 0.5f;
     private float volHighRange = 1.0f;
+    private int weaponChoice = 1;
+    private SpriteRenderer sprite;
+    
 
 
     void Awake () {
-        bulletDamage = damage;
+        bulletDamage = damagePistol;
         source = GetComponent<AudioSource>();
+        sprite = GetComponent<SpriteRenderer>();
         firePoint = transform.Find("FirePoint");
         if (firePoint == null)
         {
@@ -76,6 +88,36 @@ public class Weapon : MonoBehaviour {
             Debug.DrawLine(firePointPosition, hit.point, Color.red);
             Debug.Log("We hit " + hit.collider.name + " and did " + Damage + " damage");
         }*/
+    }
+
+    void selectWeapon()
+    {
+        weaponChoice++;
+        if (weaponChoice > 4)
+            weaponChoice = 1;
+
+        if (weaponChoice == 1)
+        {
+            bulletDamage = damagePistol;
+            sprite.sprite = handGun;
+        }
+            
+        else if (weaponChoice == 2)
+        {
+            bulletDamage = damageRifle;
+            sprite.sprite = rifle;
+        }
+            
+        else if (weaponChoice == 3)
+        {
+            bulletDamage = damageShotgun;
+            sprite.sprite = shotgun;
+        }
+        else{
+            bulletDamage = damageSniper;
+            sprite.sprite = sniper;
+        }
+
     }
 
     
