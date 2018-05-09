c03c82ba1d9856ac02d825400e2cf6daf8c412a7
victardius <37775040+victardius@users.noreply.github.com>
2018-05-09 16:06:54 +0200

    fixed dangerous objects

--- TopDownShooter/Assets/Scripts/ObjectRandom.cs
+++ TopDownShooter/Assets/Scripts/ObjectRandom.cs
@@ -1,15 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 
 public class ObjectRandom : MonoBehaviour {
 
     public GameObject[] objects = new GameObject[3];
-    public GameObject box;
-    public GameObject teslaCoil;
     public GameObject oildrum;
-    static int ammountOfFlowers = 0;
+    public GameObject teslaCoil;
+    public GameObject reactor;
+    private int amountOfTesla = 0, amountOfReactor = 0;
     private float boxScale, randXPos, randYPos;
 
     private int spawnChance = 35;
@@ -18,8 +16,8 @@ public class ObjectRandom : MonoBehaviour {
 	void Start () {
         int spawnCheck = (int)Random.Range(0,100);
         int objectSpawn = (int)Random.Range(0, 3);
-        boxScale = Random.Range(0.0f,0.3f);
-        box.transform.localScale = new Vector3(0.1f, 0.1f, 0f);
+        //boxScale = Random.Range(0.0f,0.3f);
+        //box.transform.localScale = new Vector3(0.1f, 0.1f, 0f);
         randXPos = Random.Range(0.0f, 2.0f) - 1.0f;
         randYPos = Random.Range(0.0f, 2.0f) - 1.0f;
         Vector3 position = new Vector3(this.transform.position.x + randXPos, this.transform.position.y + randYPos, this.transform.position.z);
@@ -30,22 +28,24 @@ public class ObjectRandom : MonoBehaviour {
             {
                 //Debug.Log(boxScale);
                 
-                box.transform.localScale += new Vector3(boxScale, boxScale, 0f);
-                Instantiate(this.box, position, Quaternion.identity);
-            }
-            else if(objectSpawn == 1)
-            {
+                //box.transform.localScale += new Vector3(boxScale, boxScale, 0f);
                 Instantiate(oildrum, position, Quaternion.Euler(0.0f, 0.0f, Random.Range(0, 359)));
+            }
+            else if(objectSpawn == 1 && amountOfReactor < 3)
+            {
+                amountOfReactor++;
+                Instantiate(reactor, position, Quaternion.identity);
             }
             else
             {
-                if(ammountOfFlowers < 5)
+                if(amountOfTesla < 3)
                 {
+                    amountOfTesla++;
                     Instantiate(teslaCoil, position, Quaternion.identity);
                 }
                 else
                 {
-                    Instantiate(box, position, Quaternion.identity);
+                    Instantiate(oildrum, position, Quaternion.Euler(0.0f, 0.0f, Random.Range(0, 359)));
                 }
 
             }
