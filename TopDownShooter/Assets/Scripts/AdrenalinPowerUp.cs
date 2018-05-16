c0afaee07d84888ea052db945f430f37d4213c20
Hjallimar <hjalle@ownit.nu>
2018-05-16 15:38:16 +0200

    Heroin! I mean Adrenalin

---
+++ TopDownShooter/Assets/Scripts/AdrenalinPowerUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdrenalinPowerUp : MonoBehaviour {
+
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("hitman1_gun");
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player.GetComponent<PlayerController>().increaseMovement();
+            DestroyObject(this.gameObject);
+        }
+    }
+}
