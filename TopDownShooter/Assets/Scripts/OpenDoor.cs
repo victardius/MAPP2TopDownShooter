940b06d95e3e3a610c08c4c79839bc2cbdad0072
Hjallimar <hjalle@ownit.nu>
2018-05-17 15:00:10 +0200

    Doors'n shit

---
+++ TopDownShooter/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+
+    private GameObject player;
+    public GameObject door;
+
+    private void Start()
+    {
+        player = GameObject.Find("hitman1_gun");
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+           door.GetComponent<LevelDoorScript>().openTheDoor();
+            DestroyObject(this.gameObject);
+        }
+    }
+}
