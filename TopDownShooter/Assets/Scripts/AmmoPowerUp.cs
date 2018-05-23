353e53c81562ec74a20ee5363eac6cf33bf10081
Flargy <marlun1996@hotmail.com>
2018-05-23 11:46:22 +0200

    ui

---
+++ TopDownShooter/Assets/Scripts/AmmoPowerUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPowerUp : MonoBehaviour {
+
+    private GameObject player;
+    public int ammoUpShotgun = 2;
+    public int ammoUpRifle = 10;
+
+    private void Start()
+    {
+        player = GameObject.Find("hitman1_gun");
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Weapon.shotgunAmmo += ammoUpShotgun;
+            Weapon.rifleAmmo += ammoUpRifle;
+            DestroyObject(this.gameObject);
+        }
+    }
+}
